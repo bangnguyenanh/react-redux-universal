d6ae05666aa0753c16722fdb223739c1e36f765a
Bang Nguyen Anh <crispianripley@outlook.com>
2018-01-23 13:22:47 +0700

    Remove multireducer completely

--- src/components/CounterButton/CounterButton.js
+++ src/components/CounterButton/CounterButton.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'multireducer';
-import * as counterActions from 'redux/modules/counter';
 
-@connect(
-  (state, { multireducerKey: key }) => ({ count: state.counter[key].count }),
-  (dispatch, { multireducerKey: key }) => bindActionCreators(counterActions, dispatch, key)
-)
 export default class CounterButton extends Component {
   static propTypes = {
     count: PropTypes.number.isRequired,
